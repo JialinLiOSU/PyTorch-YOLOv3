d79d42a0dcb3c9c9f39aefc3ad3c8bb9841fbf81
JialinLiOSU <32602256+JialinLiOSU@users.noreply.github.com>
2020-08-11 22:05:40 -0400

    Update train.py

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ import argparse
 import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets
-from torchvision import transformsF
+from torchvision import transforms
 from torch.autograd import Variable
 import torch.optim as optim
 
